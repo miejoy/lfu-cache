8d82920e9f4751fac806ef4b6f95b881dc4fd6a8
Raymond <raymond0huang@gmail.com>
2022-07-02 21:32:14 +0800

     包名重命名为 lfu-cache

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "LFUCache",
+    name: "lfu-cache",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -23,7 +23,6 @@ let package = Package(
             name: "LFUCache",
             dependencies: [
                 .product(name: "NIO", package: "swift-nio"),
-                .product(name: "_NIOConcurrency", package: "swift-nio"),
             ]
         ),
         .testTarget(
